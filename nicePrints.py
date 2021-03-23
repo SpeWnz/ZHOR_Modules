35b2d97e418f97d3ef2ba639532efeb6ece19edf
SpeWnz <80914893+SpeWnz@users.noreply.github.com>
2021-03-23 12:26:12 +0100

    Primi due moduli
    Aggiunti i moduli nicePrints.py e progressBar.py

---
+++ nicePrints.py
@@ -0,0 +1,33 @@
+from termcolor import colored, cprint
+
+# ========================================================================================================================================================= 
+# VARIABILI GLOBALI
+
+# Nel file principale in cui è stato importato "nicePrints.py" si deve specificare se questa variabile
+# deve essere True o False, in maniera tale che la funzione debugPrint sa se deve stampare un messaggio o no
+DEBUG = False
+
+
+# ========================================================================================================================================================= 
+
+
+
+
+# stampa di debug
+def debugPrint(message):
+	if(DEBUG):
+		print("[",end='')
+		cprint("D",'yellow',end='')
+		print("]", message)
+
+# stampa di informazione generica
+def infoPrint(message):
+		print("[",end='')
+		cprint("I",'green',end='')
+		print("]", message)
+
+# stampa di errore
+def errorPrint(message):
+    print("[",end='')
+    cprint("E",'red',end='')
+    print("] " + message)
---
+++ progressBar.py
@@ -0,0 +1,28 @@
+# data una percentuale restituisce una stringa corrispondente alla barra di caricamento
+def progressBar(percentage: int, size=1):
+	progressCharacter = "#"
+
+	progress = int((percentage/10))
+
+	bar = "["
+	for i in range(0,10):
+		if(i <= (progress - 1)):
+			bar += (progressCharacter * (size))
+		else:			
+			bar += (" " * (size))
+
+	bar += "]"
+	return bar
+
+# converte una frazione in una percentuale
+def ratioToPercentage(a: int, b: int):
+	return int((a/b) * 100)
+
+# converte una frazione in una progressBar
+def ratioToProgressBar(a: int, b: int,size=1):
+	perc = ratioToPercentage(a,b)
+	return progressBar(perc,size)
+
+# mostra in una stringa la progressBar e la percentuale
+def percentageAndProgressBar(a: int, b: int, size=1):
+	return str(ratioToProgressBar(a,b,size) + " " + str(ratioToPercentage(a,b)) + "%")
