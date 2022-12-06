39c72662e14ebb43fe2bbd0b305b5af07498fb94
SpeWnz <80914893+SpeWnz@users.noreply.github.com>
2022-12-06 20:27:45 +0100

    Create terminalUtils.py

---
+++ terminalUtils.py
@@ -0,0 +1,57 @@
+import os
+import subprocess
+
+# restituisce una lista contenente i terminali disponibili
+def getAvailableTerminals(printResults=False):
+    terminals = ['qterminal','xterm','gnome-terminal','terminator']
+
+    availableTerminals = []
+    for t in terminals:        
+        
+        try:
+            out = str(subprocess.check_output(["which", t]))[2:-3]
+            availableTerminals.append(t)
+
+            if(printResults):
+                print(t,"\r\t\t\t",out)
+
+        except:
+            if(printResults):
+                print(t,"\r\t\t\t","N/A")
+
+    return availableTerminals
+        
+
+#esegue un comando in una finestra di xterm
+# -T title
+# -e command
+# esempio: xterm -T "provaprova123" -e "python2.7 -m SimpleHTTPServer 7707"
+def spawn_xterm(command: str, windowTitle: str, workingDirectory=None,executeInBackground=True):
+    
+    if(workingDirectory is None):
+        spawnStr = 'xterm -T "{}" -e "{}"'.format(windowTitle,command)
+    else:
+        spawnStr = 'xterm -T "{}" -e "cd {} && {}"'.format(windowTitle,workingDirectory,command)
+    
+    if(executeInBackground):
+        spawnStr += " &"
+    
+    os.system(spawnStr)
+
+#esegue un comando in una finestra di qterminal
+def spawn_qterminal(command: str, workingDirectory=None,executeInBackground=True):
+    
+    if(workingDirectory is None):
+        spawnStr = 'qterminal -e "{}"'
+    else:
+        spawnStr = 'qterminal --workdir "{}" -e "{}"'
+
+    if(executeInBackground):
+        spawnStr += " &"
+
+    os.system(spawnStr)
+
+
+#esegue un comando in una finestra di terminator
+def spawn_terminator():
+    raise NotImplementedError
